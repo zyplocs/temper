1ec4a493b3e59f1ec86e04f79846eb2b698d32b8
zyplocs <zyplocked@gmail.com>
2025-12-08 17:34:35 -0600

    Initial Commit

---
+++ TempTurn/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TempTurn
+//
+//  Created by Eli J on 12/8/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TempTurn/TempTurnApp.swift
@@ -0,0 +1,17 @@
+//
+//  TempTurnApp.swift
+//  TempTurn
+//
+//  Created by Eli J on 12/8/25.
+//
+
+import SwiftUI
+
+@main
+struct TempTurnApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
